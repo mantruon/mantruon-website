83ea0c43a0e7a497d6dabf9f819212e69dd6e21c
mantruon <mantruong1598@gmail.com>
2026-05-17 06:34:28 -0700

    added mobile support

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,9 @@
 import { Link } from 'react-router-dom'
+import { useState } from 'react'
 
 export default function Navbar() {
+    const [isOpen, setIsOpen] = useState(false)
+
     return (
         <nav>
             <Link to="/" className="logo">Man Truong</Link>
@@ -9,6 +12,18 @@ export default function Navbar() {
                 <Link to="/photography">Photography</Link>
                 <Link to="/projects">Projects</Link>
             </div>
+            <button className="hamburger" onClick={() => setIsOpen(!isOpen)}>
+                <span></span>
+                <span></span>
+                <span></span>
+            </button>
+            {isOpen && (
+                <div className="mobile-menu">
+                    <Link to="/writing" onClick={() => setIsOpen(false)}>Writing</Link>
+                    <Link to="/projects" onClick={() => setIsOpen(false)}>Projects</Link>
+                    <Link to="/photography" onClick={() => setIsOpen(false)}>Photography</Link>
+                </div>
+            )}
         </nav>
     )
 }
